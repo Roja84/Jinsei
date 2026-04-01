d2fa7367cf1b0a48394757eb40a29bf583290ed5
Roja84 <53279087+Roja84@users.noreply.github.com>
2026-04-01 11:06:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -111,19 +111,37 @@ function initScroll() {
             pin: true,
             scrub: 1,
             snap: 1 / (sections.length - 1),
+			//snap: false,
             start: "top top",
             end: "+=" + totalScroll
         }
     });
 
-    sections.forEach((panel, i) => {
-        ScrollTrigger.create({
-            trigger: panel,
-            start: "top center",
-            onEnter: () => setActive(i),
-            onEnterBack: () => setActive(i)
-        });
+    //sections.forEach((panel, i) => {
+        //ScrollTrigger.create({
+            //trigger: panel,
+           // start: "top center",
+           // onEnter: () => setActive(i),
+           // onEnterBack: () => setActive(i)
+        //});
+   // });
+   
+   sections.forEach((panel, i) => {
+
+    ScrollTrigger.create({
+        trigger: panel,
+        start: "top center",
+        onEnter: () => {
+            setActive(i);
+            resetSwitcher(panel);
+        },
+        onEnterBack: () => {
+            setActive(i);
+            resetSwitcher(panel);
+        }
     });
+
+});
 
     //function setActive(index) {
         //document.querySelectorAll(".dot").forEach(d => d.classList.remove("active"));
@@ -138,16 +156,73 @@ function initScroll() {
         document.querySelectorAll("nav a")[index].classList.add("active");
     }
 
-    document.querySelectorAll(".dot").forEach((dot, i) => {
-        dot.onclick = () => {
+    //document.querySelectorAll(".dot").forEach((dot, i) => {
+        //dot.onclick = () => {
+            //gsap.to(window, {
+                //scrollTo: { y: i * window.innerHeight, autoKill: false },
+                //duration: 1
+            //});
+        //};
+    //});
+	
+	// NAV CLICK
+    document.querySelectorAll("nav a").forEach((link, i) => {
+        link.addEventListener("click", e => {
+            e.preventDefault();
             gsap.to(window, {
-                scrollTo: { y: i * window.innerHeight, autoKill: false },
-                duration: 1
+                scrollTo: {
+                    //y: i * window.innerHeight,
+					y: section,
+                    autoKill: false,
+                },
+                duration: 1,
             });
-        };
+        });
     });
 
     document.getElementById("nav-indicator").style.opacity = "1";
 
     ScrollTrigger.refresh();
+}
+
+// SWITCHERS
+
+const switchers = document.querySelectorAll(".content-switcher");
+
+switchers.forEach(switcher => {
+
+    const contents = switcher.querySelectorAll(".content");
+    const controls = switcher.querySelectorAll(".controls span");
+
+    controls.forEach(btn => {
+
+        btn.addEventListener("click", () => {
+
+            let index = btn.dataset.index;
+
+            contents.forEach(c => c.classList.remove("active"));
+            controls.forEach(c => c.classList.remove("active"));
+
+            contents[index].classList.add("active");
+            controls[index].classList.add("active");
+
+        });
+
+    });
+
+});
+
+function resetSwitcher(panel) {
+
+    let switcher = panel.querySelector(".content-switcher");
+    if (!switcher) return;
+
+    let contents = switcher.querySelectorAll(".content");
+    let controls = switcher.querySelectorAll(".controls span");
+
+    contents.forEach(c => c.classList.remove("active"));
+    controls.forEach(c => c.classList.remove("active"));
+
+    contents[0].classList.add("active");
+    controls[0].classList.add("active");
 }
